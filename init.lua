a04b53e1a3ac3fb46e29f7918c13c1cb8a203015
Wolfger Schramm <wolfger@spearwolf.de>
2024-08-05 17:11:42 +0200

    initial import

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+
+-- vim.cmd [[colorscheme minicyan]]
+vim.cmd [[colorscheme randomhue]]
+
---
+++ lua/plugins/mini.lua
@@ -0,0 +1,6 @@
+return {
+  { 'echasnovski/mini.animate', version = false },
+  { 'echasnovski/mini.base16', version = false },
+  { 'echasnovski/mini.hues', version = false },
+  { 'echasnovski/mini.completion', version = false },
+}
