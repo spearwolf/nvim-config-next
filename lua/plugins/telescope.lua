772e27fc77e774f5d215b6e04447a82bdffb2252
Wolfger Schramm <wolfger@spearwolf.de>
2024-08-06 16:43:49 +0200

    add colorscheme picker like vscode

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,5 +10,6 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Show Buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help Tags' })
+    vim.keymap.set('n', '<C-K>t', builtin.colorscheme, { desc = 'Select Colorscheme' })
   end,
 }
