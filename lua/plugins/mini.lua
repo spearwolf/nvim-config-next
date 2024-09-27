6865581c9b0123b655bb2b943285b3e821dc34d9
Wolfger Schramm <wolfger@spearwolf.de>
2024-09-27 12:59:16 +0200

    fix vscode compatibility

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,7 @@
 return {
   { 'echasnovski/mini.ai', version = false, opts = {} },
   { 'echasnovski/mini.base16', version = false, cond = not vim.g.vscode },
-  { 'echasnovski/mini.completion', version = false, opts = {} },
+  { 'echasnovski/mini.completion', version = false, cond = not vim.g.vscode },
   { 'echasnovski/mini.hues', version = false, cond = not vim.g.vscode },
-  { 'echasnovski/mini.icons', version = false, opts = {} },
+  { 'echasnovski/mini.icons', version = false, cond = not vim.g.vscode },
 }
