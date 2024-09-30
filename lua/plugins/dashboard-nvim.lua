d6a798a436e04820c995e6fdf0a0b1a0546631a6
Wolfger Schramm <wolfger@spearwolf.de>
2024-09-30 10:07:50 +0200

    remove indent_blankline and add hyper weekly dashboard

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,28 @@
+return {
+  'nvimdev/dashboard-nvim',
+  cond = not vim.g.vscode,
+  lazy = false,
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+        },
+      },
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}},
+}
