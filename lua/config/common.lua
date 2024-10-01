9abb7ff55312f8fe0df8cc07f91e746be45a2a7d
Wolfger Schramm <wolfger@spearwolf.de>
2024-10-01 10:28:15 +0200

    update

--- lua/config/common.lua
+++ lua/config/common.lua
@@ -19,4 +19,4 @@ local opt = vim.opt
 opt.timeoutlen = vim.g.vscode and 1000 or 500 -- Lower than default (1000) to quickly trigger which-key
 
 -- https://github.com/rmagatti/auto-session
-opt.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
