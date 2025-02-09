9a8394a1c4e4dde581c35f91f478ae9f1aa31499
Wolfger Schramm <wolfger@spearwolf.de>
2025-02-09 10:07:09 +0100

    fix cyberdream settings

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -4,8 +4,14 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
+    -- Set light or dark variant
+    variant = "auto", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
     -- Enable transparent background
     transparent = true,
+
+    -- Reduce the overall saturation of colours for a more muted look
+    saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
 
     -- Enable italics comments
     italic_comments = true,
@@ -14,45 +20,15 @@ return {
     hide_fillchars = false,
 
     -- Modern borderless telescope theme
-    borderless_telescope = false,
+    -- borderless_telescope = false,
+    borderless_pickers = true,
 
     -- Set terminal colors used in `:terminal`
     terminal_colors = true,
 
     -- Use caching to improve performance - WARNING: experimental feature - expect the unexpected!
     -- Early testing shows a 60-70% improvement in startup time. YMMV. Disables dynamic light/dark theme switching.
-    cache = false, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
-
-    theme = {
-        variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
-        -- highlights = {
-            -- Highlight groups to override, adding new groups is also possible
-            -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
-
-            -- Example:
-            -- Comment = { fg = "#696969", bg = "NONE", italic = true },
-
-            -- Complete list can be found in `lua/cyberdream/theme.lua`
-        -- },
-
-        -- Override a highlight group entirely using the color palette
-        -- overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
-        --     -- Example:
-        --     return {
-        --         Comment = { fg = colors.green, bg = "NONE", italic = true },
-        --         ["@property"] = { fg = colors.magenta, bold = true },
-        --     }
-        -- end,
-
-        -- Override a color entirely
-        -- colors = {
-        --     -- For a list of colors see `lua/cyberdream/colours.lua`
-        --     -- Example:
-        --     bg = "#000000",
-        --     green = "#00ff00",
-        --     magenta = "#ff00ff",
-        -- },
-    },
+    -- cache = false, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
 
     -- Disable or enable colorscheme extensions
     extensions = {
