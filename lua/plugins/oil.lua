9d220c728c2832edfa521bd5ba96733e84f50133
Wolfger Schramm <wolfger@spearwolf.de>
2025-07-31 17:21:00 +0200

    add oil plugin

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/oil.nvim',
+  cond = not vim.g.vscode,
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
