c0bd305735e4842be101a01faf76ffca8b891e91
Wolfger Schramm <wolfger@spearwolf.de>
2026-01-14 09:10:04 +0100

    update

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -2,9 +2,7 @@ if not vim.g.vscode then
   return
 end
 
-vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
-vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+local vscode = require("vscode")
 
--- vim.keymap.set("n", "<A-n>", [[<cmd>call VSCodeCall("editor.action.wordHighlight.next")<cr>]])
--- vim.keymap.set("n", "<A-p>", [[<cmd>call VSCodeCall("editor.action.wordHighlight.prev")<cr>]])
-
+-- vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
+-- vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
