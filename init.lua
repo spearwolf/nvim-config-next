35da6d9fb5dc9ba13ce292002d4bb49783f40a76
Wolfger Schramm <wolfger@spearwolf.de>
2026-05-11 08:37:16 +0200

    minimal setup

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- my personal neovim v0.12+ config
+-- created by spearwolf, may'26
+
+-- recommended setup (outside of neovim, inside your terminal):
+-- $ npm i -g neovim
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.cmd [[
+  source $HOME/.config/nvim/common.vim
+]]
+
+vim.opt.compatible = false
+vim.opt.laststatus = 3
+vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = vim.g.vscode and 1000 or 500
+
+require("config.lazy")
+
+--- lazy loaded
+
+vim.cmd [[
+  colorscheme vague
+]]
