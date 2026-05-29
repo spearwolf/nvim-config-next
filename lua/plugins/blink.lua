79892c72e984db96eb27a85c478cba5a12b4becd
Wolfger Schramm <wolfger@spearwolf.de>
2026-05-29 10:51:34 +0200

    fix blink<->tab<->copilot issues

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,12 +1,26 @@
-local cmp = require('blink.cmp')
-cmp.build():wait(60000)
+local cmp = require("blink.cmp")
+-- cmp.build():wait(60000)
 cmp.setup({
   -- See :h blink-cmp-config-keymap for defining your own keymap
-  keymap = { preset = 'super-tab' },
+  keymap = {
+    preset = "enter", -- "super-tab",
+    ["<Tab>"] = false,
+    ["<S-Tab>"] = false,
+  },
 
   appearance = {
     -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
     -- Adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'mono'
+    nerd_font_variant = "mono",
+  },
+
+  -- https://main.cmp.saghen.dev/configuration/completion.html
+  completion = {
+    list = {
+      selection = {
+        preselect = false,
+        auto_insert = false,
+      },
+    },
   },
 })
